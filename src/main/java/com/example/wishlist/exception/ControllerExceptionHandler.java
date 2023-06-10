00e311641280c1f6689c65ade69fa273f5193314
thiago_margonar <thiago_margonar@sicredi.com.br>
2023-06-10 00:57:05 -0300

    Adicionado Controller Advice;

---
+++ src/main/java/com/example/wishlist/exception/ControllerExceptionHandler.java
@@ -0,0 +1,55 @@
+package com.example.wishlist.exception;
+
+import java.util.Date;
+import java.util.Objects;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+    ErrorMessage message = new ErrorMessage(
+        HttpStatus.NOT_FOUND.value(),
+        new Date(),
+        ex.getMessage(),
+        request.getDescription(false));
+    
+    return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception ex, WebRequest request) {
+    ErrorMessage message = new ErrorMessage(
+        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+        new Date(),
+        ex.getMessage(),
+        request.getDescription(false));
+    
+    return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+    ErrorMessage message = new ErrorMessage(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            new Date(),
+            getMessageParameter(ex),
+            request.getDescription(false));
+
+    return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  private String getMessageParameter(MethodArgumentNotValidException ex) {
+    if(ex.getFieldError() != null && Objects.requireNonNull(ex.getFieldError()).getDefaultMessage() != null){
+      return Objects.requireNonNull(ex.getFieldError()).getDefaultMessage();
+    }
+    return ex.getMessage();
+  }
+}
